a26a453478e82556055e1eeb7d346a1aaf8bb5b7
ZHANGJIE982 <jancy720@163.com>
2015-10-08 14:32:14 +0800

    debug

--- lib/audited/sweeper.rb
+++ lib/audited/sweeper.rb
@@ -17,12 +17,17 @@ module Audited
 
     def before_create(audit)
       audit.user ||= current_user
+      p "======================"
       p audit.user
       audit.remote_address = controller.try(:request).try(:remote_ip)
       audit.request_uuid = request_uuid if request_uuid
     end
 
     def current_user
+      p "===========current_user start============"
+      p Audited.current_user_method
+      p controller.respond_to?(Audited.current_user_method, true)
+      p controller.send(Audited.current_user_method)
       controller.send(Audited.current_user_method) if controller.respond_to?(Audited.current_user_method, true)
     end
 
